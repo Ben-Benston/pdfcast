6aee86490a1e3808d7d2f7064ab55eb253cc73b2
Ben-Benston <burhanasikwala@gmail.com>
2026-03-08 12:36:22 +0530

    Initial Commit and File Structure

---
+++ src/types/index.tsx
@@ -0,0 +1,10 @@
+export interface Room {
+    id: string
+    room_code: string
+    pdf_url: string
+    pdf_name: string
+    current_page: number
+    zoom_level: string
+    scroll_position: number
+    created_at: string
+}
